c6c24b15c8f95f40e83f72683222e4fbd9f87292
Cybercharon <54109338+Cybercharon@users.noreply.github.com>
2021-05-09 23:56:46 -0400

    Update app.py
    added notes and got it into a dataframe

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import requests
 import pandas as pd 
 
-# Global
-ENCHANT_LVL = [1,2,3]
-# rune_list = requests.get(f'https://www.albion-online-data.com/api/v2/stats/Prices/T4_RUNE,T5_RUNE,T6_RUNE,T7_RUNE,T8_RUNE.json?locations={city}').json()
-# req = requests.get(f'https://www.albion-online-data.com/api/v2/stats/History/T4_RUNE,T5_RUNE,T6_RUNE,T7_RUNE,T8_RUNE?locations={city}')
-# rune_list = requests.get(f'https://www.albion-online-data.com/api/v2/stats/History/T8_OFF_SHIELD@2.json?locations={city}').json()
-
 class ItemType:
     def __init__(self):
         self.OH = 144
@@ -38,8 +32,11 @@ class UpgradeParts:
     def parts(self):
         return self.Rune, self.Soul, self.Relic
 
-    def full_kit_upgrade_cost(self, itemCount):
+    def full_upgrade_cost(self, itemCount):
         return (self.Rune * itemCount) + (self.Soul * itemCount) + (self.Relic * itemCount)
+    
+    def upgrade_cost(self, itemcount, toLevel, fromLevel):
+        return
 
 class AlgoTrade:
     def __init__(self, city='Bridgewatch'):
@@ -47,34 +44,57 @@ class AlgoTrade:
             self.itemType = ItemType()
             self.city = city
     
+    def make_base_item_list(self, item_list=[]):
+        query_string = ','.join(item_list)        
+        base_item_list = requests.get(f'https://www.albion-online-data.com/api/v2/stats/Prices/{query_string}.json?locations={self.city}').json()
+        return base_item_list
+
     
-    # I need to clean up this max item list
-    def make_max_item_list(self,item_list=[]):
+    def make_max_item_dict(self,item_list=[]):
         item_dict = dict()
         max_query_string = '@3,'.join(item_list)
         max_item_list = requests.get(f'https://www.albion-online-data.com/api/v2/stats/Prices/{max_query_string}.json?locations={self.city}').json()
         for item in max_item_list:
-            # if item.get('sell_price_max_date') == '0001-01-01T00:00:00':
-            #     continue
-            item_dict[item.get('item_id')] = {item.get('quality'): item.get('sell_price_max')}
+            name = item.get('item_id')
+            price = item.get('sell_price_max')
+            join_name = f"{name.split('@')[0]}_{item.get('quality')}"
+            if price > 0:
+                item_dict[join_name] = {'price': price}
         return item_dict
-
-    def full_kit_upgrade(self, item_list=[]):
-        query_string = ','.join(item_list)        
-        base_item_list = requests.get(f'https://www.albion-online-data.com/api/v2/stats/Prices/{query_string}.json?locations={self.city}').json()
-        max_item_list = self.make_max_item_list(item_list)
+#######
+#### Build a function to find how fast these things sell so you get that $$ quick son
+#######
+#######
+#### Build a function to flip from one upgrade level to another one
+#######
+#######
+#### Build a function to build furnature with a prime mule for profit
+#######
+# DOUBLE CHECK THESE SELL PRICE MAX ETC...
+# GET THE TIME INTO THE DATAFRAME AND START STORING TRENDS
+    def full_upgrade(self, item_list=[]):
+        profit_list = []
+        base_item_list = self.make_base_item_list(item_list)
+        max_item_dict = self.make_max_item_dict(item_list)
         for item in base_item_list:
-            if item.get('buy_price_max_date') == '0001-01-01T00:00:00':
-                continue
+            profit_dict = dict()
             quality = item.get('quality')
             price = item.get('sell_price_max')
             itemName = item.get('item_id')
+            joint_name = f"{itemName}_{quality}"
+            if(joint_name not in max_item_dict) or (price == 0):
+                continue
             itemLevel, upgradeItemCount = self.itemType.check(itemName)
             upgradeParts = UpgradeParts(itemLevel)
-            upgradeitemCost = upgradeParts.full_kit_upgrade_cost(upgradeItemCount)
-            total_upgrade_cost = upgradeitemCost + price            
-            import ipdb
-            ipdb.set_trace() 
+            upgradeitemCost = upgradeParts.full_upgrade_cost(upgradeItemCount)
+            total_upgrade_cost = upgradeitemCost + price
+            max_price = max_item_dict[joint_name].get('price')
+            profit = max_price - total_upgrade_cost
+            if profit > 0:
+                profit_dict.update({"Item": joint_name, "Base": price, "Upgrade Cost": total_upgrade_cost, "Max Sell": max_price, "Profit": profit})
+                profit_list.append(profit_dict)
+                print({"Item": joint_name, "Base": price, "Upgrade Cost": total_upgrade_cost, "Max Sell": max_price, "Profit": profit})
+        df = pd.DataFrame(profit_list)
 
 
 
@@ -82,6 +102,9 @@ class AlgoTrade:
 
 if __name__ == '__main__':
     trader = AlgoTrade()
-    trader.full_kit_upgrade(item_list=['T4_OFF_BOOK','T4_OFF_DEMONSKULL_HELL','T4_OFF_LAMP_UNDEAD','T4_OFF_JESTERCANE_HELL'])
+    # make this list just items no T# then check T4-T6 etc... 
+    # add capturing time to this
+
+    trader.full_upgrade(item_list=['T4_OFF_BOOK','T4_OFF_DEMONSKULL_HELL','T4_OFF_LAMP_UNDEAD','T4_OFF_JESTERCANE_HELL', 'T4_OFF_HORN_KEEPER', 'T4_OFF_ORB_MORGANA', 'T4_OFF_TORCH', 'T4_OFF_TOTEM_KEEPER'])
 
 
